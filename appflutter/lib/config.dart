40a767b66f255e3c8181f9044cfe4d7bc416d616
gabosavan <gabonavasus@gmail.com>
2023-04-11 23:32:59 -0400

    primer commit del frontentd en flutter

---
+++ appflutter/lib/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const String appName = "RENTATUYATE";
+  static const String apiURL = '192.168.1.108';
+  static const productosAPI = "api/producto";
+  static const clientesAPI = "api/cliente";
+  static const loginAPI = "api/login/";
+  static const obtenertokenAPI = "api/api-token-auth/";
+}
